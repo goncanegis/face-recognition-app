ed8b273dd8719483457420f3bf689974d3ab8863
goncanegis <gonca.negis@gmail.com>
2022-02-18 12:07:05 +0300

    component styling and background

--- src/App.js
+++ src/App.js
@@ -1,17 +1,115 @@
+import React, { Component } from 'react';
 import './App.css';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import Particles from 'react-tsparticles';
 
-function App() {
-  return (
-    <div className="App">
-      <Navigation />
-      <Logo />
-      <ImageLinkForm />
-      {/*  <FaceRecognition /> */}
-    </div>
-  );
+const particleOptions = {
+  fpsLimit: 120,
+  interactivity: {
+    events: {
+      onClick: {
+        enable: true,
+        mode: 'push',
+      },
+      onHover: {
+        enable: true,
+        mode: 'repulse',
+      },
+      resize: true,
+    },
+    modes: {
+      bubble: {
+        distance: 400,
+        duration: 2,
+        opacity: 0.8,
+        size: 40,
+      },
+      push: {
+        quantity: 4,
+      },
+      repulse: {
+        distance: 200,
+        duration: 0.4,
+      },
+    },
+  },
+  particles: {
+    color: {
+      value: '#ffffff',
+    },
+    links: {
+      color: '#ffffff',
+      distance: 150,
+      enable: true,
+      opacity: 0.4,
+      width: 1,
+    },
+    collisions: {
+      enable: true,
+    },
+    move: {
+      direction: 'none',
+      enable: true,
+      outMode: 'bounce',
+      random: true,
+      speed: 3,
+      straight: false,
+    },
+    number: {
+      density: {
+        enable: true,
+        area: 800,
+      },
+      value: 100,
+    },
+    opacity: {
+      value: 0.4,
+    },
+    shape: {
+      type: 'circle',
+    },
+    size: {
+      random: true,
+      value: 5,
+    },
+  },
+  detectRetina: true,
+};
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+    };
+  }
+
+  onInputChange = (e) => {
+    console.log(e.target.value);
+  };
+
+  onButtonSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Particles id="tsparticles" options={particleOptions} />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm
+          onInputChange={this.onInputChange}
+          onButtonSubmit={this.onButtonSubmit}
+        />
+        {/*  <FaceRecognition /> */}
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,18 +1,28 @@
 import React from 'react';
 import './ImageLinkForm.css';
 
-const ImageLinkForm = () => {
+const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
   return (
-    <div>
+    <div className="flex flex-column items-center" id="formContainer">
       <p className="f3">
         {'This Magic Brain will detect faces in your pictures. Give it a try!'}
       </p>
-      <div className="flex flex-column items-center">
-        <input className="f4 mb3 pa2 w-70 center" type="text" />
-        <button className="w-30 grow f4 link ph3 pv2 dib white bg-light-purple">
+      <form
+        onSubmit={onButtonSubmit}
+        className=" form flex justify-center items-center w-80 pa4 br2 shadow-4"
+      >
+        <input
+          className="f4 pa2 w-70 center bn br2"
+          type="text"
+          onChange={onInputChange}
+        />
+        <button
+          onClick={onButtonSubmit}
+          className="w-30 pointer grow f4 link ph3 pv2 dib white bg-light-purple bn hover-bg-purple br2"
+        >
           Detect
         </button>
-      </div>
+      </form>
     </div>
   );
 };
